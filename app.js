c02a4d68a085d3a861a300057ad034d930d72a74
Manuel Valles <manukempo@gmail.com>
2018-02-19 15:24:59 +0000

    Check the Guess - Messages & Style

--- app.js
+++ app.js
@@ -25,7 +25,37 @@ guessBtn.addEventListener('click', function(){
   // Validate
   if(isNaN(guess) || guess < min || guess > max){
     setMessage(`Please enter a number between ${min} and ${max}`, 'red');
+  } else {
+
+    // Check the Guess
+    if(guess === winningNum){
+      // Game Over - Won
+      // Disable input
+      guessInput.disabled = true;
+      // Change border color
+      guessInput.style.borderColor = 'green';
+      // Set message
+      setMessage(`${winningNum} is correct, You WIN!`, 'green');
+    } else{
+      // Wrong number
+      guessesLeft -= 1;
+      if(guessesLeft === 0){
+        // Game Over - Lost
+        // Disable input
+        guessInput.disabled = true;
+        // Change border color
+        guessInput.style.borderColor = 'red';
+        // Set message
+        setMessage(`Game Over, you lost. The correct number was ${winningNum}`, 'red');
+      } else{
+        // Game Continues - Wrong Answer
+        // Set message
+        setMessage(`${guess} is not correct. ${guessesLeft} guesses left`, 'red');
+      }
+    }
+
   }
+
 });
 
 // Set message
